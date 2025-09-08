cdb403ff45e6c9ee45dd0dad7218e6fd3593e453
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2025-09-09 03:45:05 +0900

    feat: zodのスキーマ生成

--- client/orval.config.ts
+++ client/orval.config.ts
@@ -1,23 +1,25 @@
 export default {
   api: {
-    // 1. 入力となるOpenAPI仕様書のパス
     input: '../docs/openapi.swagger.json',
-
     output: {
-      // 2. 'tags-split'モードで、APIのタグごとにファイルを分割する
       mode: 'tags-split',
-      // 3. 生成されるフックやAPIクライアントの出力先
-      target: './app/shared/api/endpoints',
-      // 4. TanStack Query v5 用のカスタムフックを生成
+      target: './app/api/gen',
       client: 'react-query',
-      // 5. Zodのバリデーションスキーマも生成する
       validation: true,
     },
-
-    // 6. Zodスキーマの生成に関する詳細設定
-    zod: {
-      // Zodスキーマの出力先
-      output: './app/shared/api/schemas',
-    }
+    hooks: {
+      afterAllFilesWrite: "pnpm biome:check",
+    },
+  },
+  zod: {
+    input: {
+      target: '../docs/openapi.swagger.json',
+    },
+    output: {
+      mode: 'tags-split',
+      client: 'zod',
+      target: 'app/schema/gen',
+      fileExtension: '.zod.ts',
+    },
   },
 };
