180692339121f25606358d70aa06dd627307509c
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2025-11-22 03:13:54 +0900

    feat: media serviceのファイル作成

---
+++ server/services/media/cmd/main.go
@@ -0,0 +1,73 @@
+package cmd
+
+import (
+	"context"
+	"net"
+	"os"
+	"shared/logger"
+	"time"
+
+	pb "chat-app-proto/gen/user"
+
+	"github.com/go-playground/validator"
+	"github.com/joho/godotenv"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+var db *pgxpool.Pool
+
+func init() {
+	_ = godotenv.Load()
+
+	log := logger.Default("user-service")
+	dsn := os.Getenv("DATABASE_URL")
+
+	log.Info("Connecting to database...")
+	var err error
+	for i := 0; i < 30; i++ {
+		db, err = pgxpool.New(context.Background(), dsn)
+		if err == nil {
+			break
+		}
+		log.Warn("Failed to connect to database, retrying...", "attempt", i+1, "error", err)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		log.Error("Failed to connect to database after retries", "error", err)
+		os.Exit(1)
+	}
+	log.Info("Connected to PostgreSQL", "database", "chat_app")
+}
+
+func main() {
+	log := logger.Default("user-service")
+	defer func() {
+		db.Close()
+	}()
+	userRepo := postgres.NewPostgresUserRepository(gen.New(db))
+	validate := validator.New()
+	userUsecase := usecase.NewUserUsecase(userRepo, usecase.Config{
+		JWTSecret: os.Getenv("JWT_SECRET"),
+	}, validate)
+	userHandler := handler.NewUserHandler(userUsecase, log)
+
+	server := grpc.NewServer()
+	pb.RegisterUserServiceServer(server, userHandler)
+	reflection.Register(server)
+
+	port := 50051
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Error("Failed to listen", "port", port, "error", err)
+		os.Exit(1)
+	}
+
+	log.Info("User service starting", "port", port)
+	if err := server.Serve(lis); err != nil {
+		log.Error("Failed to serve", "error", err)
+		os.Exit(1)
+	}
+}
---
+++ server/services/media/internal/handler/media_handler.go
@@ -0,0 +1,5 @@
+package handler
+
+type MediaRepository interface {
+	GenerateUploadURL
+}
