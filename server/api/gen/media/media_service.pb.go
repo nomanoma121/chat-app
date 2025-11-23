6abe5410a3ef810a1dc7e08a907bfed6cf444f72
nomanoma121 <takeuchi.k.747@ms.saitama-u.ac.jp>
2025-11-23 13:35:52 +0900

    feat: protoを修正

--- server/api/gen/media/media_service.pb.go
+++ server/api/gen/media/media_service.pb.go
@@ -7,6 +7,8 @@
 package mediapb
 
 import (
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2/options"
+	_ "google.golang.org/genproto/googleapis/api/annotations"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -73,6 +75,7 @@ func (MediaType) EnumDescriptor() ([]byte, []int) {
 type GetPresignedUploadURLRequest struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	MediaType     MediaType              `protobuf:"varint,1,opt,name=media_type,json=mediaType,proto3,enum=media.MediaType" json:"media_type,omitempty"`
+	Filename      string                 `protobuf:"bytes,2,opt,name=filename,proto3" json:"filename,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -112,6 +115,13 @@ func (x *GetPresignedUploadURLRequest) GetMediaType() MediaType {
 		return x.MediaType
 	}
 	return MediaType_MEDIA_TYPE_UNSPECIFIED
+}
+
+func (x *GetPresignedUploadURLRequest) GetFilename() string {
+	if x != nil {
+		return x.Filename
+	}
+	return ""
 }
 
 type GetPresignedUploadURLResponse struct {
@@ -162,19 +172,23 @@ var File_media_service_proto protoreflect.FileDescriptor
 
 const file_media_service_proto_rawDesc = "" +
 	"\n" +
-	"\x13media_service.proto\x12\x05media\"O\n" +
+	"\x13media_service.proto\x12\x05media\x1a.protoc-gen-openapiv2/options/annotations.proto\x1a\x1cgoogle/api/annotations.proto\"\x8a\x01\n" +
 	"\x1cGetPresignedUploadURLRequest\x12/\n" +
 	"\n" +
-	"media_type\x18\x01 \x01(\x0e2\x10.media.MediaTypeR\tmediaType\">\n" +
+	"media_type\x18\x01 \x01(\x0e2\x10.media.MediaTypeR\tmediaType\x12\x1a\n" +
+	"\bfilename\x18\x02 \x01(\tR\bfilename:\x1d\x92A\x1a\n" +
+	"\x18\xd2\x01\n" +
+	"media_type\xd2\x01\bfilename\">\n" +
 	"\x1dGetPresignedUploadURLResponse\x12\x1d\n" +
 	"\n" +
 	"upload_url\x18\x01 \x01(\tR\tuploadUrl*\\\n" +
 	"\tMediaType\x12\x1a\n" +
 	"\x16MEDIA_TYPE_UNSPECIFIED\x10\x00\x12\x19\n" +
 	"\x15MEDIA_TYPE_GUILD_ICON\x10\x01\x12\x18\n" +
-	"\x14MEDIA_TYPE_USER_ICON\x10\x022r\n" +
+	"\x14MEDIA_TYPE_USER_ICON\x10\x022\xb3\x01\n" +
 	"\fMediaService\x12b\n" +
-	"\x15GetPresignedUploadURL\x12#.media.GetPresignedUploadURLRequest\x1a$.media.GetPresignedUploadURLResponseBc\n" +
+	"\x15GetPresignedUploadURL\x12#.media.GetPresignedUploadURLRequest\x1a$.media.GetPresignedUploadURLResponse\x1a?\x92A<\n" +
+	"\x05Media\x123Media service for handling media-related operationsBc\n" +
 	"\tcom.mediaB\x11MediaServiceProtoP\x01Z\x0f./media;mediapb\xa2\x02\x03MXX\xaa\x02\x05Media\xca\x02\x05Media\xe2\x02\x11Media\\GPBMetadata\xea\x02\x05Mediab\x06proto3"
 
 var (
